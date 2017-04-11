b79090fb2f652d946bf619c967bc9378a16bbc02
jeetsinghparmar <jeetparmar949@gmail.com>
2017-04-12 00:46:22 +0530

    added spring security basic authentication

---
+++ src/main/java/com/mightyjava/config/SpringSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.mightyjava.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private AuthenticationEntryPoint entryPoint;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	} 
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
+			.authenticationEntryPoint(entryPoint);
+	}
+
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}*/
+	
+}
